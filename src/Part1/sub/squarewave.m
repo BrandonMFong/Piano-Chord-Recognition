ffad3eedbec2639254808a24d86fc7c1d8318c81
BrandonMFong <fong.m.brandon97@gmail.com>
2020-11-25 09:25:14 -0800

    finish square wave

---
+++ src/Part1/sub/squarewave.m
@@ -0,0 +1,21 @@
+% Example 4: Square Wave
+Fs = 150;
+t = 0:1/Fs:1;
+f = 5;
+x = square(2*pi*t*f);
+nfft = 1024;
+X = fft(x,nfft);
+X = X(1:nfft/2);
+mx = abs(X);
+f = (0:nfft/2-1) * Fs/nfft;
+fig();
+plot(t,x);
+title('Square Wave Sig');
+xlabel('Time (s)');
+ylabel('Amplitude');
+ylim([-2 2]);
+fig();
+plot(f,mx);
+title('Power Spectrum of Square Sig');
+xlabel('Frequency (Hz)');
+ylabel('Power');
