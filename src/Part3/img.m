ba888273cf11e2a402bea613467cf0e7e13191db
BrandonMFong <fong.m.brandon97@gmail.com>
2020-11-29 10:41:38 -0800

    reorganizing the script

--- src/Part3/img.m
+++ src/Part3/img.m
@@ -3,25 +3,33 @@
 
 lena_read = imread(const.Part3.Step1.ImageFile); % read img and convert to a double matrix 
 lena = im2double(lena_read); % read img and convert to a double matrix 
+fig();imshow(lena);
 
 % get the dct matrix
 D = dctmtx(8);
 
-% process image
+% get mask matrix
+mtx = csvread(const.Part3.Step1.maskFile); % read the mask, don't want to do this all the time
+mask = @(block_struct) mtx .* block_struct.data;
+
+% process original image
 dct = @(block_struct) D * block_struct.data * D';
-B = blockproc(lena,[8 8], dct); 
+
+% process compressed image
+invdct = @(block_struct) D' * block_struct.data * D;
+
+
+
+B = blockproc(lena(:,:,3),[8 8], dct); 
 
 % apply mask on dct coefficients
 % this is applying compression because we don't need those high freq components 
-mask = @(block_struct) csvread(const.Part3.Step1.maskFile) .* block_struct.data;
 B2 = blockproc(B,[8 8], mask);
 
 % not sure what this is for 
 non_zero_B = length (find(B));
 non_zero_B2 = length (find(B2));
 
-invdct = @(block_struct) D' * block_struct.data * D;
 lena_compressed = blockproc(B2,[8 8], invdct);
 
-fig();imshow(lena);
 fig();imshow(lena_compressed);
