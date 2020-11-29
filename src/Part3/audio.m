a58e9efd21f012c7b4f9a924471cc5742dbc9ecb
BrandonMFong <fong.m.brandon97@gmail.com>
2020-11-29 14:38:22 -0800

    finish part 3 and now onto part 4

---
+++ src/Part3/audio.m
@@ -0,0 +1,46 @@
+% Step 2
+% I am going to use the bfong1.wav used in Part 2 of this project
+% Nevermind, strong.mat is already preloaded
+% Example link: https://www.mathworks.com/help/signal/ug/dct-for-speech-signal-compression.html
+clear dct;
+load('strong.mat');
+
+% her audio
+x = her'; % get the inverse matrix of her
+X = dct(x); % get the DCT 
+
+[XX,ind] = sort(abs(X), 'descend');
+
+need = 1;
+while norm(X(ind(1:need)))/norm(X)<0.999
+    need = need + 1;
+end
+
+xpc = need / length(X)*100;
+
+X(ind(need+1:end)) = 0; % zeroing out the coefficients with 0.1% of energy 
+xx = idct(X);
+
+figure;
+plot([x;xx;x - xx]');
+legend('Original', [int2str(xpc) '% of coeffs.'], 'Difference', 'Location', 'best');
+
+
+% him audio 
+y = him';
+Y = dct(y);
+
+[YY, ind] = sort(abs(Y),'descend');
+
+need = 1;
+while norm(Y(ind(1:need)))/norm(Y)<0.999
+    need = need + 1;
+end
+
+ypc = need/length(Y)*100;
+Y(ind(need+1:end)) = 0;
+yy = idct(Y);
+
+figure;
+plot([y;yy;y-yy]')
+legend('Original',[int2str(ypc) '% of coeffs.'], 'Difference', 'Location', 'best');
