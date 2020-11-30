8ba4a2d7e9a156859f9d425236ca4f3c5eb4120a
BrandonMFong <fong.m.brandon97@gmail.com>
2020-11-30 08:43:31 -0800

    the notes of the chords are not being realized, the third not does get here

--- src/Part4/ChordRecog.m
+++ src/Part4/ChordRecog.m
@@ -6,41 +6,53 @@ sprintf('NOTE:')
 [y,fs] = audioread(const.Part4.Chord.AudioFile);
 ydft = fft(y);
 
-% Gets the positive coefficients
 ydft = abs(ydft(1:length(y)/2+1));
 
-% x axis
 freq = 0:fs/length(y):fs/2;
 
-% plot magnitude
 plot(freq,ydft);
-xlim([0 2000])
+xlim([0 200])
 xlabel('Hz');
 ylabel('Amplitude');
 
-maxval = 0; % variable that will hold the first max freq value over the amplitude threshold
-amp = 0; % var to hold the max amplitude 
-len = length(freq); % getting length of the frequency vector 
-chordlength = const.Part4.Chord.Length;
-thresh = const.Part4.Chord.AmplitudeThreshold; % read threshold for amplitude
+% init chord vars
+chordlength = const.Part4.Chord.Length; % Length of the chord, just doing three at first 
+chord = [0 0 0]; % init the var that holds the freq
+chorditr = 1; % index for the chord array 
+
+maxval = 0; % I wont need this since I am using chord
+amp = 0; 
+len = length(freq);
+thresh = const.Part4.Chord.AmplitudeThreshold; 
+
+% find frequency of chords
 for itr = 1:len
-    if(ydft(itr) > thresh) % if we are over the threshold, don't want the low noise signal amp
+    if(ydft(itr) > thresh) 
+
+        % if the amplitude is greater than the last iteration, then save the frequency and update the amp var
         if(ydft(itr) > amp) % test to see if there is a max
-            maxval = freq(itr);
+            chord(chorditr) = freq(itr);
             amp = ydft(itr);
         else 
-            break; % get out of the loop if we are decreasing
+            % break; % get out of the loop if we are decreasing
+            if (chorditr > 3)
+                break;
+            end
         end
+
+        % if the index is 0, then we haven't found the next note
+        % but if it is filled, that means we found a frequency
+        if(chord(chorditr) ~= 0)
+            chorditr = chorditr + 1;
+        end 
     end
+
 end
 
-% Load the notes table given by https://pages.mtu.edu/~suits/notefreqs.html 
 notes = readtable(const.Part4.Chord.NotesTable);
 
-% I am getting the first max freq coefficient and using that to determine the what note it is
 note_freq = table2array(notes(:,2))';
 [val,idx] = min(abs(note_freq-maxval));
 
-% using the index of the first max val on the notes table
 sprintf('   - Chord from .wav file is: %s', string(notes(idx,1).Var1))
 
